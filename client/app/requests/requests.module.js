4b9958c37f2127c29319dec06ab7d089deb87a42
Aparna Karve <akarve@redhat.com>
2017-01-20 16:48:28 -0800

    Changes per PR #450
    Add explicit import and export, convert app scripts to modules

--- client/app/requests/requests.module.js
+++ client/app/requests/requests.module.js
@@ -5,7 +5,13 @@ import { ProcessRequestsModalComponent } from './process-requests-modal/process-
 import { RequestExplorerComponent } from './request-explorer/request-explorer.component.js';
 import { RequestListComponent } from './request-list/request-list.component.js';
 import { RequestsStateFactory } from './requests-state.service.js';
+<<<<<<< cf8c3407d45dd9702204716ac980824ed36880b4
 import { SharedModule } from '../shared/shared.module.js';
+=======
+import { RequestWorkflowComponent } from './request-workflow/request-workflow.component.js';
+import { SharedModule } from '../shared/shared.module.js';
+import { TagsTreeComponent } from './request-workflow/tags-tree.component.js';
+>>>>>>> Changes per PR #450
 
 export const RequestsModule = angular
   .module('app.requests', [
@@ -16,6 +22,11 @@ export const RequestsModule = angular
   .component('requestExplorer', RequestExplorerComponent)
   .component('orderExplorer', OrderExplorerComponent)
   .component('requestList', RequestListComponent)
+<<<<<<< cf8c3407d45dd9702204716ac980824ed36880b4
+=======
+  .component('requestWorkflow', RequestWorkflowComponent)
+  .component('tagsTree', TagsTreeComponent)
+>>>>>>> Changes per PR #450
   .factory('OrdersState', OrdersStateFactory)
   .factory('RequestsState', RequestsStateFactory)
   .name;
